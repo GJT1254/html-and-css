57202f81956ef2cb8f1eab9e05898c44e9dbb07e
GJT1254 <120106180+GJT1254@users.noreply.github.com>
2022-12-22 19:44:11 +0530

    Initial Commit

---
+++ day-13/server.js
@@ -0,0 +1,22 @@
+var somename = 200045;
+console.log(somename);
+
+//string = ``,"",'';
+
+var forname ="vhcshdghdgd";
+console.log(forname);
+
+var flag= false;
+console.log(false)
+
+var forarray=[1,2,3,"gdhhjcjhds",true];
+console.log(typeof(forarray));
+console.log (forarray);
+//data types ia java Script
+  //var
+  //number
+  //Boolean
+  //Array
+  //Undefined
+  //String
+  //Object
